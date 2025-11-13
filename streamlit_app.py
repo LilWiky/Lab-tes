5916820c73f5c7fcc7724fc1187484142e77fec1
LilWiky <jaymeang0205@gmail.com>
2025-11-13 14:56:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 hello im jayme")
+st.title("🎈 hello world")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
